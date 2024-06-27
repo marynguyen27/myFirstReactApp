4960b533fbd12afe13577f9e733169e23daef689
Mary Nguyen <marynguyen27@gmail.com>
2024-06-26 20:35:29 -0700

    map over puppies array

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import { useState } from 'react';
 import './App.css';
+import { puppyList } from './data.js';
 
 function App() {
-  return <></>;
+  console.log(puppyList);
+
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log(puppies);
+
+  return (
+    <div className='App'>
+      {puppies.map((puppy) => {
+        return <p key={puppy.id}>{puppy.name}</p>;
+      })}
+    </div>
+  );
 }
 
 export default App;
