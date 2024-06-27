e0ca53bda09fbe2fbf6b739392885a50323b34f7
Mary Nguyen <marynguyen27@gmail.com>
2024-06-26 20:57:46 -0700

    CSS

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,39 @@ function App() {
 
   console.log(puppies);
 
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
+  function handleClick(puppyId) {
+    setFeatPupId(puppyId);
+    console.log(`Puppy with ID ${puppyId} was clicked`);
+  }
+
   return (
     <div className='App'>
-      {puppies.map((puppy) => {
-        return <p key={puppy.id}>{puppy.name}</p>;
-      })}
+      {puppies.map((puppy) => (
+        <p
+          onClick={() => {
+            console.log('puppy id: ', puppy.id);
+            setFeatPupId(puppy.id);
+          }}
+          key={puppy.id}
+          className='puppy-name'
+        >
+          {puppy.name}
+        </p>
+      ))}
+      {featPupId && (
+        <div className='featured-puppy'>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
